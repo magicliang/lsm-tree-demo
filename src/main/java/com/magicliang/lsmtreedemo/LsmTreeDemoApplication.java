40f7cff29e60c5045917fdc2df203a3119441d4a
magicliang <liangchuan@meituan.com>
2021-01-24 18:37:57 +0800

    init project

---
+++ src/main/java/com/magicliang/lsmtreedemo/LsmTreeDemoApplication.java
@@ -0,0 +1,13 @@
+package com.magicliang.lsmtreedemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LsmTreeDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LsmTreeDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/magicliang/lsmtreedemo/LsmTreeDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.magicliang.lsmtreedemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LsmTreeDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
